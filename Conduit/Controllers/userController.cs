54bc04b7cbb6946da95454998f774d04cc014bf2
Aisha Marmash <aishamarmash@gmail.com>
2022-06-12 05:35:56 +0300

    Get current user & update user

---
+++ Conduit/Controllers/userController.cs
@@ -0,0 +1,91 @@
+﻿using AutoMapper;
+using Conduit.Data;
+using Conduit.Data.Repositories;
+using Conduit.Domain.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace Conduit.Controllers
+{
+    [Route("api/user")]
+    [ApiController]
+    public class userController : Controller
+    {
+        UserContext userContext;
+        private IConfiguration _config;
+        UserRepository userRepo;
+        private readonly IMapper _mapper;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public userController(IConfiguration config, IMapper mapper, IHttpContextAccessor httpContextAccessor)
+        {
+            userContext = new();
+            _config = config;
+            userRepo = new UserRepository(userContext);
+            _config = config;
+            _mapper = mapper;
+            _httpContextAccessor = httpContextAccessor;
+        }
+        [Authorize]
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var token = GetCurrentAsync();
+            var tokenn = new JwtSecurityTokenHandler().ReadJwtToken(token);
+            var claim = tokenn.Claims.First(c => c.Type == "email").Value;
+            var result = userRepo.Find(claim);
+            UserForResponse userResponse = _mapper.Map<UserForResponse>(result);
+            userResponse.Token = token;
+            return Ok(userResponse);
+        }
+        //[Authorize]
+        //[HttpPatch]
+        //public ActionResult user([FromBody]JsonPatchDocument<UserForUpdateDto>patchDocument)
+        //{
+        //    if (patchDocument == null)
+        //    {
+        //        return BadRequest();
+        //    }
+        //    var token = GetCurrentAsync();
+        //    var tokenn = new JwtSecurityTokenHandler().ReadJwtToken(token);
+        //    var claim = tokenn.Claims.First(c => c.Type == "email").Value;
+        //    var userFromRepo = userRepo.Find(claim);
+        //    if (userFromRepo == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    var userToPatch = _mapper.Map<UserForUpdateDto>(userFromRepo);
+        //    patchDocument.ApplyTo(userToPatch);
+        //    _mapper.Map(userToPatch, userFromRepo);
+        //    userRepo.UpdateUser(claim, userFromRepo);
+        //    return NoContent();
+        //}
+        [Authorize]
+        [HttpPut]
+        public ActionResult Updateuser(UserForUpdateDto userForUpdateDto)
+        {
+            var token = GetCurrentAsync();
+            var tokenn = new JwtSecurityTokenHandler().ReadJwtToken(token);
+            var claim = tokenn.Claims.First(c => c.Type == "email").Value;
+            var userFromRepo = userRepo.Find(claim);
+            if (userFromRepo == null)
+            {
+                return NotFound();
+            }
+            userRepo.UpdateUser(userFromRepo, userForUpdateDto);
+            return NoContent();
+        }
+        [NonAction]
+        private string GetCurrentAsync()
+        {
+            var authorizationHeader = _httpContextAccessor.HttpContext.Request.Headers["authorization"];
+
+            return authorizationHeader == StringValues.Empty
+                ? string.Empty
+                : authorizationHeader.Single().Split(" ").Last();
+        }
+    }
+}
